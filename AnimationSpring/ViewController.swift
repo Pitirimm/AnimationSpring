fdddb70b398e039f59332e731c804bb1f281f5e0
Yuri Martinovsky <vvrite@me.com>
2021-06-23 12:29:44 +0300

    Added animation

--- AnimationSpring/ViewController.swift
+++ AnimationSpring/ViewController.swift
@@ -9,15 +9,49 @@ import UIKit
 import Spring
 
 class ViewController: UIViewController {
-    @IBOutlet weak var viewLabel: SpringView!
+    @IBOutlet weak var viewSpring: SpringView!
+    @IBOutlet weak var buttonSpring: SpringButton!
+    @IBOutlet weak var viewLabel: UILabel!
+    @IBOutlet weak var viewNextLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    let animation = ["shake", "pop", "morph", "squeeze", "wobble", "swing", "flipX", "flipY", "fall", "squeezeLeft", "squeezeRight", "squeezeDown", "squeezeUp"]
+    let curve = ["spring", "linear", "easeIn", "easeOut", "easeInOut"]
+    var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        viewSpring.layer.cornerRadius = 12
+        buttonSpring.layer.cornerRadius = 12
+    }
+    
+    func playAnimation() {
+        if index < animation.count {
+            viewSpring.animation = animation[index]
+            viewSpring.curve = curve.randomElement() ?? "spring"
+            viewLabel.text = "Current animation: \(animation[index])"
+            settingsLabel.text = "Curve: \(viewSpring.curve)"
+            index += 1
+           
+            if index < animation.count {
+                viewNextLabel.text = "Next animation: \(animation[index])"
+            } else {
+                index = 0
+                viewNextLabel.text = "Next animation: \(animation[index])"
+            }
+            
+        } else {
+            index = 0
+            viewSpring.animation = animation[index]
+            viewLabel.text = "Current animation: \(viewSpring.animation)"
+        }
     }
 
     @IBAction func pressedButton(_ sender: SpringButton) {
+        buttonSpring.animation = "pop"
+        playAnimation()
+        viewSpring.animate()
+        buttonSpring.animate()
     }
     
 }
-
